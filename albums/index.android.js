ca060d5a3b8b2ced29ffe325b30461bbda8976c8
salman18 <salmanaleemkhan786@gmail.com>
2017-09-19 12:48:26 +0530

    albums

---
+++ albums/index.android.js
@@ -0,0 +1,11 @@
+
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+const App = () => (
+  <Header headerText={'Albums!'} />
+);
+
+AppRegistry.registerComponent('albums', () => App);
